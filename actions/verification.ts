c65df03e996399efa4aa635e8f557029f65a16b0
Bachtiarazizy <bachtiarazizy@gmail.com>
2024-07-26 21:41:31 +0300

    add

--- actions/verification.ts
+++ actions/verification.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { ZodStringDef, z } from "zod";
+import { z } from "zod";
 import { redirect } from "next/navigation";
 import prisma from "@/lib/db";
 
@@ -15,11 +15,11 @@ export type State = {
 
 const VerificationDataSchema = z.object({
   name: z.string().min(3, { message: "The name has to be a minimum character length of 3" }),
-  photoUrl: z.array(z.string(), { message: "Images are required" }),
   email: z.string().email({ message: "Email is required" }),
   address: z.string().min(10, { message: "Address is required" }),
-  identityDocument: z.array(z.string().min(10, { message: "ID is required" })),
-  studentCard: z.array(z.string().min(10, { message: "Student ID is required" })),
+  identityNumber: z.string().min(10, { message: "Student ID is required" }),
+  photoUrl: z.array(z.string(), { message: "Images are required" }),
+  studentDocument: z.array(z.string().min(10, { message: "ID is required" })),
   phoneNumber: z.string().min(10, { message: "Phone is required" }),
 });
 
@@ -35,9 +35,9 @@ export async function verificationData(prevState: any, formData: FormData) {
     name: formData.get("name"),
     email: formData.get("email"),
     address: formData.get("address"),
+    identityNumber: formData.get("identityNumber"),
     photoUrl: JSON.parse(formData.get("photoUrl") as string),
-    studentCard: JSON.parse(formData.get("studentCard") as string),
-    identityDocument: JSON.parse(formData.get("identityDocument") as string),
+    studentDocument: JSON.parse(formData.get("studentDocument") as string),
     phoneNumber: formData.get("phoneNumber"),
   });
 
@@ -51,14 +51,14 @@ export async function verificationData(prevState: any, formData: FormData) {
     return state;
   }
 
-  const data = await prisma.verificationData.create({
+  await prisma.verificationData.create({
     data: {
       name: validateFields.data.name,
       email: validateFields.data.email,
       address: validateFields.data.address,
       photoUrl: validateFields.data.photoUrl,
-      studentCard: validateFields.data.studentCard,
-      identityDocument: validateFields.data.identityDocument,
+      identityNumber: validateFields.data.identityNumber,
+      studentDocument: validateFields.data.studentDocument,
       phoneNumber: validateFields.data.phoneNumber,
       userId: user.id,
     },
