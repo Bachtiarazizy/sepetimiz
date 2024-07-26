de612424e1bfbd2e632b01780009f6c8c009f896
Bachtiarazizy <bachtiarazizy@gmail.com>
2024-07-26 16:56:25 +0300

    filtesr

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
-export const page = () => {
-  return (
-    <div>page</div>
-  )
+export default function page() {
+  return <div>page</div>;
 }
