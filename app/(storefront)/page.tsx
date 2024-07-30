fa6c6c8625ec6391eff410a0bf29f42cad808f88
Bachtiarazizy <bachtiarazizy@gmail.com>
2024-07-30 14:14:38 +0300

    assskak

--- app/(storefront)/page.tsx
+++ app/(storefront)/page.tsx
@@ -1,20 +1,8 @@
 import { MarqueeDemo } from "@/components/Animation/Marquee";
 import Reviews from "@/components/Animation/Reviews";
-import Categories from "@/components/Categories";
 import Banner from "@/components/hyper-ui/Banner";
 import Marketing from "@/components/hyper-ui/Marketing";
-import { MarqueeDemoVertical } from "@/components/hyper-ui/Review";
-import ProductResults from "@/components/products/ProductResult";
 import { ProductRow } from "@/components/products/ProductRow";
-
-import SelectCategories from "@/components/products/SelectCategories";
-import ProductFilterSidebar from "@/components/shared/ProductFilterSidebar";
-import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
-import { ProductFilterValues } from "@/lib/zodSchemas";
-import { ListFilter, Search } from "lucide-react";
-import { Metadata } from "next";
 
 export default function Home() {
   return (
