70e57bc0fb8e1dcda765d46475e6bd46677372ee
Bachtiarazizy <bachtiarazizy@gmail.com>
2024-08-08 18:07:30 +0300

    test

--- app/api/send/send.ts
+++ app/api/send/send.ts
@@ -1,8 +1,65 @@
+// import React from "react";
+// import { Resend } from "resend";
+// import { getErrorMessage, validateString } from "@/lib/email";
+// import ContactFormEmail from "@/components/products/ProductsEmails";
+// import VerificationEmail from "@/components/products/VerificationEmail";
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export async function sendEmail(formData: FormData) {
+//   const senderEmail = formData.get("senderEmail");
+//   const message = formData.get("message");
+
+//   // Simple server-side validation
+//   if (!validateString(senderEmail, 500)) {
+//     return { error: "Invalid sender email" };
+//   }
+//   if (!validateString(message, 5000)) {
+//     return { error: "Invalid message" };
+//   }
+
+//   let data;
+//   try {
+//     data = await resend.emails.send({
+//       from: "Sepetimiz <onboarding@resend.dev>",
+//       to: "sepetimiz.bk@gmail.com",
+//       subject: "Message from contact form",
+//       reply_to: senderEmail,
+//       react: React.createElement(ContactFormEmail, {
+//         message: message,
+//         senderEmail: senderEmail,
+//       }),
+//     });
+//   } catch (error: unknown) {
+//     return { error: getErrorMessage(error) };
+//   }
+
+//   return { data };
+// }
+
+// export async function sendVerificationEmail(userEmail: string, userName: string) {
+//   let data;
+//   try {
+//     data = await resend.emails.send({
+//       from: "Sepetimiz <onboarding@resend.dev>",
+//       to: userEmail,
+//       subject: "Account Verification Successful",
+//       react: React.createElement(VerificationEmail, { userName }),
+//     });
+//   } catch (error: unknown) {
+//     return { error: getErrorMessage(error) };
+//   }
+
+//   return { data };
+// }
+
+"use server";
+
 import React from "react";
 import { Resend } from "resend";
-import { getErrorMessage, validateString } from "@/lib/email";
+
 import ContactFormEmail from "@/components/products/ProductsEmails";
-import VerificationEmail from "@/components/products/VerificationEmail";
+import { getErrorMessage, validateString } from "@/lib/email";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -10,12 +67,16 @@ export async function sendEmail(formData: FormData) {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
-  // Simple server-side validation
+  // simple server-side validation
   if (!validateString(senderEmail, 500)) {
-    return { error: "Invalid sender email" };
+    return {
+      error: "Invalid sender email",
+    };
   }
   if (!validateString(message, 5000)) {
-    return { error: "Invalid message" };
+    return {
+      error: "Invalid message",
+    };
   }
 
   let data;
@@ -31,24 +92,12 @@ export async function sendEmail(formData: FormData) {
       }),
     });
   } catch (error: unknown) {
-    return { error: getErrorMessage(error) };
+    return {
+      error: getErrorMessage(error),
+    };
   }
 
-  return { data };
-}
-
-export async function sendVerificationEmail(userEmail: string, userName: string) {
-  let data;
-  try {
-    data = await resend.emails.send({
-      from: "Sepetimiz <onboarding@resend.dev>",
-      to: userEmail,
-      subject: "Account Verification Successful",
-      react: React.createElement(VerificationEmail, { userName }),
-    });
-  } catch (error: unknown) {
-    return { error: getErrorMessage(error) };
-  }
-
-  return { data };
+  return {
+    data,
+  };
 }
