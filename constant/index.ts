668c7b14cb74a3f60888369c1a65e7a004fa71ed
Bachtiarazizy <bachtiarazizy@gmail.com>
2024-11-23 14:29:03 +0300

    cores

--- constant/index.ts
+++ constant/index.ts
@@ -16,6 +16,59 @@ export const NavLinks = [
   { href: "/Dashboard/sell-product", id: 3, name: "Sell Your Product" },
   { href: "/customer-service", id: 4, name: "Customer Service" },
 ];
+export const NavLinks = [
+  { href: "/", id: 0, name: "Home" },
+  { href: "/about-us", id: 1, name: "About us" },
+  { href: "/search", id: 2, name: "Find Product" },
+  { href: "/shops", id: 3, name: "Sell Your Product" },
+  { href: "/customer-service", id: 4, name: "Customer Service" },
+];
+
+export const categoryFilters = [
+  { name: "Fashions", href: "/products/fashion" },
+  { name: "Electronics", href: "/products/electronics" },
+  { name: "Food", href: "/products/food" },
+  { name: "Bagages", href: "/products/baggages" },
+  { name: "Exchanges", href: "/products/exchanges" },
+  { name: "All", href: "/products/all" },
+];
+
+export const footerLinks = [
+  {
+    title: "About Us",
+    links: [
+      { id: 1, name: "Company Profile ", href: "/about-us" },
+      { id: 2, name: "Our Team", href: "/about-us" },
+      { id: 3, name: "Our Values", href: "/about-us" },
+      { id: 4, name: "Contact Us", href: "/customer-service" },
+    ],
+  },
+  {
+    title: "Our Services",
+    links: [
+      { id: 1, name: "Sell on sepetimiz", href: "Dashboard/sell-product" },
+      { id: 2, name: "Buy Products", href: "/products/all" },
+      { id: 3, name: "Affiliate program", href: "/customer-service" },
+      { id: 4, name: "Advertise your store", href: "/customer-service" },
+    ],
+  },
+  {
+    title: "Helpful Links",
+    links: [
+      { id: 1, name: "Help Center", href: "/customer-service" },
+      { id: 2, name: "Contact Us", href: "/customer-service" },
+      { id: 3, name: "FAQs", href: "/customer-service" },
+    ],
+  },
+  {
+    title: "Contact Us",
+    links: [
+      { id: 1, name: "sepetimiz.bk@gmail.com", href: "mailto:sepetimiz.bk@gmail.com" },
+      { id: 2, name: "+90 555 444 33 22", href: "tel:+905421793483" },
+      { id: 3, name: "Lane, Istanbul, Turkey", href: "#" },
+    ],
+  },
+];
 
 export const categoryFilters = [
   { name: "Fashions", href: "/products/fashion" },
