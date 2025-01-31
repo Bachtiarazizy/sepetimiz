222b0d00135b17a187571f76f837934bcc435644
Bachtiarazizy <bachtiarazizy@gmail.com>
2025-01-31 12:29:44 +0300

    suspens

--- app/(storefront)/customer-service/page.tsx
+++ app/(storefront)/customer-service/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from "react";
 import { MessageCircle, Mail, ChevronDown, Users, ShoppingCart, Store, HelpCircle, Shield, BookOpen } from "lucide-react";
 
